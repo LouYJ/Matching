da6c1b9b61110dc6317dd546ccb2f3a93451c144
Yujing Lou <865227010@qq.com>
2017-10-18 19:50:24 +0800

    Add function to get all objects from one scene

--- testply.py
+++ testply.py
@@ -4,6 +4,8 @@ import csv
 import pandas as pd
 from plyfile import PlyData, PlyElement
 
+# This file is used to extract all objects from one scene
+
 def load_json_file(path):
 	with open(path) as json_file:
 		jsondata = json.load(json_file)
@@ -16,7 +18,9 @@ def dump_json_file(data, path):
 def read_ply_file(path):
 	with open(path, 'rb') as ply_file:
 		plydata = PlyData.read(ply_file)
-		return plydata
+		vertex = plydata['vertex']
+		face = plydata['face']
+		return plydata, vertex, face
 
 def create_ply_file(vertex, path, text=False):
 	vertex = np.array(vertex, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1'), ('alpha', 'u1'), ('label', 'u4')])
@@ -31,20 +35,37 @@ def get_segments(segsjson):
 	segIndices = segsjson['segIndices']
 	segments = {}
 	for v in segIndices:
-		segs[v] = []
+		segments[v] = []
 
 	for i in range(len(segIndices)):
-		segs[segIndices[i]].append(i)
+		segments[segIndices[i]].append(i)
 	return segments
 
 def get_object(seglist, segments, vertex):
 	tmp = []
 	obj = []
 	for seg in seglist:
-		tmp.extend(segmentsp[seg])
+		tmp.extend(segments[seg])
+	# print (tmp)
 	for v in tmp:
-		obj.append(vertex[tmp])
+		obj.append(vertex[v])
+	return obj
 
+def get_objects_from_scene(sceneId):
+	segsjson = load_json_file('scene' + sceneId + '_vh_clean_2.0.010000.segs.json')
+	aggjson = load_json_file('scene' + sceneId + '_vh_clean.aggregation.json')
+	_, vertex, face = read_ply_file('scene' + sceneId + '_vh_clean_2.labels.ply')
+	
+	segments = get_segments(segsjson)
+	segGroups = aggjson['segGroups']
+	for id in segGroups:
+		seglist = id['segments']
+		obj = get_object(seglist, segments, vertex)
+		# print (obj)
+		create_ply_file(obj, 'obj' + str(id['id']) + '.ply', True)
+
+
+'''
 def get_seg_part(plydata):
 	vertex = plydata['vertex']
 
@@ -57,11 +78,6 @@ def get_seg_part(plydata):
 		seg[v[7]].append(v)
 	return seg
 
-
-	
-
-
-'''
 def get_object_seg(jsondata, plydata):
 	face = plydata['face']
 	vertex = plydata['vertex']
@@ -98,9 +114,7 @@ def main():
 	# create_ply_file(seg['segGroups'][47]['vertices'], 'out47.ply', text = True)
 
 if __name__ == "__main__": 
-	main()
-
-	# read_csv_file('/Users/Loj/Desktop/scannet-labels.combined.tsv')
+	get_objects_from_scene('0000_00')
 
 
 
