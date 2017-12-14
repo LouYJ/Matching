7ae90bfea374c5f1e10c68f792f81f92114e3268
Yujing Lou <865227010@qq.com>
2017-12-14 11:43:01 +0800

    transformv0.1

--- transform.py
+++ transform.py
@@ -25,9 +25,10 @@ def read_taxonomy(path):
 			dictName[n] = element['synsetId']
 	return dictName
 
+def obj_to_h5(file):
+	
 
-
-def transform_ply_to_h5(path, nameList):
+def transform_shapenet_to_h5(path, nameList):
 	dictName = read_taxonomy(path + 'taxonomy.json')
 	nameList = read_name(path + 'shape_names.txt')
 	if not (os.path.exits('./ShapeNet_h5')):
@@ -36,3 +37,9 @@ def transform_ply_to_h5(path, nameList):
 	for name in nameList:
 		if (dictName.has_key(name)):
 			synsetId = dictName[name]
+			idPath = path + 'ShapeNetCore.v2/' + synsetId + '/'
+			dirList = os.listdir(idPath)
+			for d in dirList:
+				file = idPath + d + '/model/model_normalized.obj'
+				if (os.path.exists(file)):
+					pass
